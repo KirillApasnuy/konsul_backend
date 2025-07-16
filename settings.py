9a785fd9b5a92c3b2a28ea28e55674c1aa877d69
Kirill Apasnuy <bybnjnugyvynn@gmail.com>
2025-07-16 22:11:53 +0800

    full first commit1

--- settings.py
+++ settings.py
@@ -7,8 +7,6 @@ class Settings():
         self.ES_HOST = "http://elasticsearch:9200"
         self.ES_INDEX = "documents"
         self.ES_INDEX_SS = "konsul_cases"
-        self.DEEPSEEK_API_KEY = "sk-2219ca020f024cdca3e02017be2b37c3"
         self.DEEPSEEK_URL = "https://api.deepseek.com"
-        self.OPENAI_API_KEY = "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"
         self.EDMY_FLASK_URL = "http://147.45.135.236:8443"
         self.EDMY_FLASK_API_KEY = "aWxvdmVzdXp1a2k="
