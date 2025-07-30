cf5ccafa48d7ab8c65dd7410ff3c7a948a4fb339
Kirill Apasnuy <bybnjnugyvynn@gmail.com>
2025-07-30 14:02:06 +0800

    fix settings

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,5 @@
 class Settings:
-    ES_HOST = "http://localhost:9200"
+    ES_HOST = "http://elasticsearch:9200"
     ES_INDEX = "court_decisions"
     GEMINI_API_KEY = "AIzaSyC-rOs5RxAWrmZFYpkBjbon5qYdCqto-cc"
     GEMINI_MODEL = "gemini-2.5-flash"
