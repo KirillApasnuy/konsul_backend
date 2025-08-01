594bb86282df0db75fb1abad1f65c78dbc0e1de6
Kirill Apasnuy <bybnjnugyvynn@gmail.com>
2025-08-01 16:37:07 +0800

    update prompt

--- app/settings.py
+++ app/settings.py
@@ -15,6 +15,20 @@ class Settings:
                             "Используй юридически точные формулировки."
                             "ТОЛЬКО ЭТО, БОЛЬШЕ НИЧЕГО НЕ НАДО")
     prm = {
-  "prompt": "You are an experienced lawyer. Analyze the case law on a given legal topic. Prioritize the case law of the Supreme Court of the Russian Federation, then selected appellate courts for the most suitable case, and finally courts of first instance, viewing them as a single chain in proving the decisions of courts of various instances.\n\n### Key Documents and Evidence:\n* List the types of documents used by the parties to support their claims or objections, with references from the parties to these documents.\n\n### Legally Significant Circumstances to be Proved in the Dispute:\n* Indicate the circumstances and the causal link between them that are used when rendering a decision to satisfy or deny claims on the given topic.\n\n### Examples from Case Practice:\n* Provide examples of court decisions in similar cases, highlighting the case law of the Supreme Court of the Russian Federation with all stages of the cases, then selected appellate courts for the most suitable cases, and finally courts of first instance. Include links to documents."
+  "prompt": "You are an experienced lawyer. "
+            "Analyze the case law on a given legal topic. "
+            "Prioritize the case law of the Supreme Court of the Russian Federation, "
+            "then selected appellate courts for the most suitable case, "
+            "and finally courts of first instance, viewing them as a single chain "
+            "in proving the decisions of courts of various instances.\n\n### "
+            "Key Documents and Evidence:\n* List the types of documents used by the "
+            "parties to support their claims or objections, with references from the"
+            " parties to these documents.\n\n### Legally Significant Circumstances to be"
+            " Proved in the Dispute:\n* Indicate the circumstances and the causal link between t"
+            "hem that are used when rendering a decision to satisfy or deny claims on the give"
+            "n topic.\n\n### Examples from Case Practice:\n* Provide examples of court decisions"
+            " in similar cases, highlighting the case law of the Supreme Court of the Russian Federa"
+            "tion with all stages of the cases, then selected appellate courts for the most suitable cases, and finally courts of first instance. "
+            "###Be sure to turn off links to documents."
 }
     ANALYZE_LEGAL_PROMPT = str(prm)
